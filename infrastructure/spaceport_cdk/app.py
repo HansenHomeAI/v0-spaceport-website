e4ccaee1f2234128afd7621b4aa51ae265237424
Gabriel Hansen <gbhbyu@gmail.com>
2025-05-12 23:45:18 -0800

    Enhance CDK deployment workflow by updating the deploy step to include context for AWS account and region. Refactor app.py to utilize these context values for environment configuration in SpaceportStack.

--- infrastructure/spaceport_cdk/app.py
+++ infrastructure/spaceport_cdk/app.py
@@ -3,5 +3,10 @@ from aws_cdk import App
 from spaceport_cdk.spaceport_stack import SpaceportStack
 
 app = App()
-SpaceportStack(app, "SpaceportStack")
+SpaceportStack(app, "SpaceportStack",
+    env={
+        'account': app.node.try_get_context('account') or None,
+        'region': app.node.try_get_context('region') or 'us-west-2'
+    }
+)
 app.synth() 
