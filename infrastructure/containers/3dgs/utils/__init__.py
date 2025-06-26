f439d85dedb947e6627aac4f1a62add6700dd297
Gabriel Hansen <gbhbyu@gmail.com>
2025-06-26 17:47:10 -0600

    Fix import error in utils/__init__.py: remove unused colmap_loader import
    - Remove unused import of read_colmap_scene from utils/__init__.py
    - This was the remaining source of the ImportError after fixing train_gaussian_production.py
    - Now both direct import and utils package import are clean
    
    Progress: This should finally resolve the 4.5-minute import error barrier

--- infrastructure/containers/3dgs/utils/__init__.py
+++ infrastructure/containers/3dgs/utils/__init__.py
@@ -7,6 +7,5 @@ from .loss import Loss
 from .model import GaussianModel
 from .visualizer import Visualizer
 from .dataset import SpaceportDataset
-from .colmap_loader import read_colmap_scene
 
-__all__ = ['logger', 'Loss', 'GaussianModel', 'Visualizer', 'SpaceportDataset', 'read_colmap_scene'] 
+__all__ = ['logger', 'Loss', 'GaussianModel', 'Visualizer', 'SpaceportDataset'] 
