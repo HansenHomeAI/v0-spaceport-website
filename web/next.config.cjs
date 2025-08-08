b06765ab66733824a4620a9811fc360acdfce53e
Gabriel Hansen <gbhbyu@gmail.com>
2025-08-07 21:31:42 -0600

    ci: add CF Pages and GH Pages deploy; export config toggle; restore / redirect

--- web/next.config.cjs
+++ web/next.config.cjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
+const output = process.env.NEXT_OUTPUT_EXPORT ? 'export' : undefined;
 const nextConfig = {
   reactStrictMode: true,
   experimental: { appDir: true },
   images: { unoptimized: true },
+  output,
   basePath,
   assetPrefix: basePath ? `${basePath}/` : undefined,
 };
