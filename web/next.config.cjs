8e6af69157cfad77fa903ded0c679469780ec1b3
Gabriel Hansen <gbhbyu@gmail.com>
2025-08-08 13:03:32 -0600

    ci/web: fix CF deploy target; add redirect / -> /landing in Next.js config

--- web/next.config.cjs
+++ web/next.config.cjs
@@ -1,7 +1,12 @@
-/** @type {import(next).NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: { appDir: true },
   images: { unoptimized: true },
+  async redirects() {
+    return [
+      { source: '/', destination: '/landing', permanent: false },
+    ];
+  },
 };
 module.exports = nextConfig;
