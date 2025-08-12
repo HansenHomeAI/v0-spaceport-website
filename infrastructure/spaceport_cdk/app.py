cbbab63be533ab29061f242682a97f4caabc3970
Gabriel Hansen <gbhbyu@gmail.com>
2025-08-12 15:03:46 -0600

    chore(auth): split Cognito into dedicated AuthStack; remove legacy Cognito from SpaceportStack; add v2 pool/client/invite API with distinct names

--- infrastructure/spaceport_cdk/app.py
+++ infrastructure/spaceport_cdk/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from aws_cdk import App
 from spaceport_cdk.spaceport_stack import SpaceportStack
+from spaceport_cdk.auth_stack import AuthStack
 from spaceport_cdk.ml_pipeline_stack import MLPipelineStack
 
 app = App()
@@ -15,6 +16,14 @@ spaceport_stack = SpaceportStack(app, "SpaceportStack",
 
 # Deploy the new ML pipeline stack
 ml_pipeline_stack = MLPipelineStack(app, "SpaceportMLPipelineStack",
+    env={
+        'account': app.node.try_get_context('account') or None,
+        'region': app.node.try_get_context('region') or 'us-west-2'
+    }
+)
+
+# Deploy dedicated Auth stack (v2) to avoid legacy pool immutability
+auth_stack = AuthStack(app, "SpaceportAuthStack",
     env={
         'account': app.node.try_get_context('account') or None,
         'region': app.node.try_get_context('region') or 'us-west-2'
