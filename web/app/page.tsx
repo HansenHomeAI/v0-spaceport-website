95449fd395bac4c0a401b01b4ab8e317c3237bd8
Gabriel Hansen <gbhbyu@gmail.com>
2025-09-02 17:49:54 -0600

    Trigger rebuild - test authentication with migrated users

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,4 +1,4 @@
 // ci: noop change to trigger development pipeline
-// Cloudflare Pages redeployment trigger - 2025-01-27 15:45:00 UTC
+// Cloudflare Pages redeployment trigger - 2025-01-27 16:30:00 UTC - test authentication with migrated users
 export const runtime = 'edge';
 export { default } from './landing/page';
