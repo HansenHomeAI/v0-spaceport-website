44fd5e51aa2a16433e5f6f52a239dd49fffb6668
Gabriel Hansen <gbhbyu@gmail.com>
2025-09-04 00:29:32 -0600

    Trigger rebuild with beta access API URL secret
    - GitHub secret BETA_ACCESS_API_URL_PREVIEW now configured
    - Next deployment will include the environment variable
    - Beta access admin UI should appear for authorized users

--- web/app/create/page.tsx
+++ web/app/create/page.tsx
@@ -383,3 +383,4 @@ export default function Create(): JSX.Element {
 }
 
 // Beta access admin system deployed - Thu Sep  4 00:26:19 MDT 2025
+// Beta access API URL secret added - Thu Sep  4 00:29:32 MDT 2025
