39964d135555e77cc8cbe5d6678a02be1ddd1fef
Gabriel Hansen <gbhbyu@gmail.com>
2026-04-29 14:59:50 -0600

    chore: add md1 v18 pipeline viewer handoff

---
+++ web/app/pipeline-viewer/page.tsx
@@ -0,0 +1,7 @@
+export const runtime = "edge";
+
+import SogsViewerDevPanel from "../../components/sogs-viewer/SogsViewerDevPanel";
+
+export default function PipelineViewerPage() {
+  return <SogsViewerDevPanel />;
+}
