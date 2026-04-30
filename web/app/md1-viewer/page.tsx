5d209189934eeec5e2788acceca4e5f9d94769bf
Gabriel Hansen <gbhbyu@gmail.com>
2026-04-30 11:59:28 -0600

    fix: run md1 viewer on edge runtime

--- web/app/md1-viewer/page.tsx
+++ web/app/md1-viewer/page.tsx
@@ -1,5 +1,7 @@
 import Md1ProductionViewer from "../../components/md1-viewer/Md1ProductionViewer";
 
+export const runtime = "edge";
+
 export default function Md1ViewerPage() {
   return <Md1ProductionViewer />;
 }
